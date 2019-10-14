46632ad046b8419f16242659491add1e517183c5
bouzuya <m@bouzuya.net>
2019-10-14 19:30:05 +0900

    remove tag create feature from tag fragment

--- app/src/main/java/net/bouzuya/sample5/TagViewModel.kt
+++ app/src/main/java/net/bouzuya/sample5/TagViewModel.kt
@@ -5,16 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import org.threeten.bp.Instant
-import org.threeten.bp.ZoneOffset
-import org.threeten.bp.format.DateTimeFormatter
 
 class TagViewModel(private val _tagRepository: TagRepository) : ViewModel() {
     private val _editTagEvent = MutableLiveData<Event<Tag>>()
     val editTagEvent: LiveData<Event<Tag>> = _editTagEvent
-
-    private val _nameText = MutableLiveData<String>()
-    val nameText: LiveData<String> = _nameText
 
     private val _tagList = MutableLiveData<List<Tag>>()
     val tagList: LiveData<List<Tag>> = _tagList
@@ -25,21 +19,6 @@ class TagViewModel(private val _tagRepository: TagRepository) : ViewModel() {
 
     fun edit(tag: Tag) {
         _editTagEvent.value = Event(tag)
-    }
-
-    fun updateNameText(s: String) {
-        _nameText.value = s
-    }
-
-    fun insert() = viewModelScope.launch {
-        val name = _nameText.value ?: return@launch
-        val createdAt = Instant.now().atZone(ZoneOffset.UTC).toOffsetDateTime()
-            .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-        val tag = Tag(0, name, createdAt)
-        _tagRepository.insert(tag)
-
-        _nameText.value = ""
-        refreshList()
     }
 
     fun refresh() {
