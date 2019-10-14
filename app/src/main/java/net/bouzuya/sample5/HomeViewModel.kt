b66d84b70ebf083fa8990325d4ea3fe237de2f97
bouzuya <m@bouzuya.net>
2019-10-14 19:22:46 +0900

    remove bookmark create feature from home

--- app/src/main/java/net/bouzuya/sample5/HomeViewModel.kt
+++ app/src/main/java/net/bouzuya/sample5/HomeViewModel.kt
@@ -2,9 +2,6 @@ package net.bouzuya.sample5
 
 import androidx.lifecycle.*
 import kotlinx.coroutines.launch
-import org.threeten.bp.Instant
-import org.threeten.bp.ZoneOffset
-import org.threeten.bp.format.DateTimeFormatter
 
 class HomeViewModel(private val _bookmarkRepository: BookmarkRepository) : ViewModel() {
 
@@ -16,12 +13,6 @@ class HomeViewModel(private val _bookmarkRepository: BookmarkRepository) : ViewM
 
     private val _bookmarkCount = MutableLiveData<Int>()
     val bookmarkCount: LiveData<String> = Transformations.map(_bookmarkCount) { it.toString() }
-
-    private val _nameText = MutableLiveData<String>()
-    val nameText: LiveData<String> = _nameText
-
-    private val _urlText = MutableLiveData<String>()
-    val urlText: LiveData<String> = _urlText
 
     private val _bookmarkList = MutableLiveData<List<Bookmark>>()
     val bookmarkList: LiveData<List<Bookmark>> = _bookmarkList
@@ -46,30 +37,8 @@ class HomeViewModel(private val _bookmarkRepository: BookmarkRepository) : ViewM
         _goToTagEvent.value = Event(Unit)
     }
 
-    fun insert() = viewModelScope.launch {
-        val name = _nameText.value ?: return@launch
-        val url = _urlText.value ?: return@launch
-        val createdAt = Instant.now().atZone(ZoneOffset.UTC).toOffsetDateTime()
-            .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-        val bookmark = Bookmark(0, name, url, createdAt)
-        _bookmarkRepository.insert(bookmark)
-
-        _nameText.value = ""
-        _urlText.value = ""
-
-        refreshList()
-    }
-
     fun refresh() = viewModelScope.launch {
         refreshList()
-    }
-
-    fun updateNameText(s: String) {
-        _nameText.value = s
-    }
-
-    fun updateUrlText(s: String) {
-        _urlText.value = s
     }
 
     private suspend fun refreshList() {
