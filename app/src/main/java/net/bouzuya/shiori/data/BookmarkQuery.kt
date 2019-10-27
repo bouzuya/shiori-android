2daa4b3e18be39900c7655b08c51f62238b46ac0
bouzuya <m@bouzuya.net>
2019-10-27 14:42:10 +0900

    support some words query

--- app/src/main/java/net/bouzuya/shiori/data/BookmarkQuery.kt
+++ app/src/main/java/net/bouzuya/shiori/data/BookmarkQuery.kt
@@ -1,6 +1,18 @@
 package net.bouzuya.shiori.data
 
-data class BookmarkQuery(private val query: String) {
+data class BookmarkQuery(private val queryString: String) {
+    private val predicateList: List<(bookmarkWithTagList: BookmarkWithTagList) -> Boolean> by lazy {
+        if (queryString.isEmpty()) emptyList()
+        else {
+            queryString.split(" ").filter { it.isNotEmpty() }.map { word ->
+                { bookmarkWithTagList: BookmarkWithTagList ->
+                    word.isEmpty()
+                            || bookmarkWithTagList.bookmark.name.contains(word, ignoreCase = true)
+                }
+            }
+        }
+    }
+
     fun match(bookmarkWithTagList: BookmarkWithTagList): Boolean =
-        query.isEmpty() || bookmarkWithTagList.bookmark.name.contains(query, ignoreCase = true)
+        predicateList.isEmpty() || predicateList.all { it(bookmarkWithTagList) }
 }
